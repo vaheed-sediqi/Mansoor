cfbc87a607f3216d3f924273dc9d07e11113f19a
vaheed.sediqi <vaheed.sediqi@gmail.com>
2019-01-20 15:45:11 +0300

    stiky navbar

--- home/js/main.js
+++ home/js/main.js
@@ -7,4 +7,17 @@ jQuery(document).ready(function(){
     });
        
     
+
+    // stiky navbar
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        if(top>=60){
+          $("nav").addClass('secondary');
+        }
+
+        else 
+            if($("nav").hasClass('secondary')) {
+                $("nav").removeClass('secondary');
+        }
+    });
 });
